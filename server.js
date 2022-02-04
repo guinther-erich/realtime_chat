98ec749b1834aa384a60a4b9ee8a9cff39f018bd
guinther-erich <guinther.erich@gmail.com>
2022-02-04 07:33:47 -0300

    Server corrections

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const app = require("express")();
 const httpServer = require("http").createServer(app);
-//const options = { /* ... */ };
-const io = require("socket.io")(httpServer);
-const PORT = 3000;
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+const PORT = 7000;
 
-io.on("connection", socket => {
-    console.log("Conectado com o id do Socket" + socket.id);
+const users = {};
+
+io.on("connection", (socket) => {
+    console.log("Conectado com o id do Socket " + socket.id);
+
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} se desconectou.`);
+
+        for(let user in users){
+            if(users[user] === socket.id){
+                delete users[user];
+            }
+        }
+
+        io.emit("guests.all", users);
+    });
+
+    socket.on("guest.new", (username) => {
+        console.log("New guest: " + username);
+        users[username] = socket.id;
+
+        io.emit("guests.all", users);
+    });
+
+    socket.on("message.send", (data) => {
+        console.log(data);
+
+        const socketId = users[data.receiver];
+        io.to(socketId).emit("message.new", data);
+    });
 });
 
 httpServer.listen(PORT, () => {
